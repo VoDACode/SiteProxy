f52e6821e2f2a8a090e76560d93771196bee458e
VoDA <dimavovk201112@gmail.com>
2022-10-02 20:12:57 +0300

    Improvement proxy

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express");
-const config = require('config-dynamic');
+const configDynamic = require('config-dynamic');
 const { tcpPingPort } = require('tcp-ping-port');
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const path = require('path');
 const mainApp = express();
 const errorProxy = express();
 
-let servers = [];
+let config = configDynamic.util.loadFileConfigs(path.join(__dirname, "config"));
+
+let servers = new Map();
 checkServersStatus();
-setInterval(checkServersStatus, 10000);
-config.util.loadFileConfigs(".\\config");
+
+setInterval(checkServersStatus, 5000);
 
 mainApp.use(
     "/",
@@ -18,20 +20,20 @@ mainApp.use(
         ws: true,
         router: async function (req) {
             const host = req.hostname;
-            const routers = config.get('routers');
+            const routers = config.routers;
 
             if (!routers || !Array.isArray(routers))
                 throw new Error('Incorrect config parameter "routers"');
 
             const item = routers.find(p => p.from == host);
-            if (!item) {
+            if (!item || servers.size == 0) {
                 req.headers["code"] = 404;
-                return `http://${host}:${config.get('ProxyErrorsServerPort')}/`;
+                return `http://${host}:${config.ProxyErrorsServerPort}/`;
             }
 
-            if (!servers.find(p => p.server == item.from).status) {
+            if (servers.size > 0 && servers.has(item.from) && !servers.get(item.from)) {
                 req.headers["code"] = 500;
-                return `http://${host}:${config.get('ProxyErrorsServerPort')}/`;
+                return `http://${host}:${config.ProxyErrorsServerPort}/`;
             }
 
             return item.to;
@@ -39,50 +41,47 @@ mainApp.use(
     })
 );
 
-if (config.get("api.isEnabled")) {
+if (config.api.isEnabled) {
     errorProxy.get("/_api/proxy-list", (req, res) => {
-        if (req.query.apiKey != "" && req.query.apiKey != config.get("api.key")) {
+        if (req.query.apiKey != "" && req.query.apiKey != config.api.key) {
             res.statusCode = 401;
             res.end();
             return;
         }
-        res.json(servers);
+        let results = [];
+        for (let server of servers) {
+            results.push({ host: server[0], status: server[1] });
+        }
+        res.json(results);
     });
 }
 
-errorProxy.use("/", (req, res) => {
+errorProxy.get("/", (req, res) => {
     if (req.headers["code"] == '404')
-        res.sendFile(path.join(__dirname, '/404.html'));
+        res.sendFile(path.join(__dirname, '404.html'));
     else if (req.headers["code"] == '500')
-        res.sendFile(path.join(__dirname, '/500.html'));
+        res.sendFile(path.join(__dirname, '500.html'));
     else
-        res.sendFile(path.join(__dirname, '/404.html'));
+        res.sendFile(path.join(__dirname, '404.html'));
 });
 
-mainApp.listen(config.get('ProxyServePort'), () => {
-    console.log(`Starting Proxy at *:${config.get('ProxyServePort')}`);
+mainApp.listen(config.ProxyServePort, () => {
+    console.log(`Starting Proxy at *:${config.ProxyServePort}`);
 });
 
-errorProxy.listen(config.get('ProxyErrorsServerPort'), () => {
-    console.log(`Starting ErrorProxy at *:${config.get('ProxyErrorsServerPort')}`);
+errorProxy.listen(config.ProxyErrorsServerPort, () => {
+    console.log(`Starting ErrorProxy at *:${config.ProxyErrorsServerPort}`);
 })
 
 function checkServersStatus() {
-    const routers = config.get('routers');
+    const routers = config.routers;
     routers.forEach(async (element) => {
-        let isNew = false;
-        if (!servers.find(p => p.server == element.from))
-            isNew = true;
         let tmp = element.to.replace("http://", "").replace("https://", "").split(':');
         let status = await tcpPingPort(tmp[0], Number(tmp[1]), {
             socketTimeout: 10000,
             dnsTimeout: 10000,
             dnsServers: ['8.8.8.8']
         });
-        if (isNew) {
-            servers.push({ server: element.from, status: status.online })
-        } else {
-            servers.find(p => p.server == element.from).status = status.online;
-        }
+        servers.set(element.from, status.online);
     });
 }
